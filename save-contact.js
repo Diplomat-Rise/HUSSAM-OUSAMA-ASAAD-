f00e71da6425b79770b6c1249dd33f9c81903db8
Diplomat-Rise <diploriseit@gmail.com>
2025-06-30 16:46:32 +0400

    Update save-contact.js

--- save-contact.js
+++ save-contact.js
@@ -3,15 +3,15 @@
 //   const vCardData = `
 // BEGIN:VCARD
 // VERSION:3.0
-// FN:Afaf Mohammad Marouf
+// FN:HUSSAM OUSAMA ASAAD
 // TITLE:Sales Manager
 // ORG:RISE Properties
-// TEL;TYPE=WORK,VOICE:+971588982647
-// TEL;TYPE=WORK,VOICE:+971547172318
+// TEL;TYPE=WORK,VOICE:+971525572118‬‬‬
+// TEL;TYPE=WORK,VOICE:+971589826204
 // TEL;TYPE=WORK,VOICE:+97145665109
-// EMAIL:Hiba@riseproperties.ae
+// EMAIL:Hussam@riseproperties.ae
 // URL:http://www.riseproperties.ae
-// ADR;TYPE=WORK:;;PO Box No 455979, Office NO.1607, Opal Tower, Business Bay;Dubai;;UAE
+// ADR;TYPE=WORK:;;PO Box No 181702, Office NO.1607, Opal Tower, Business Bay;Dubai;;UAE
 // END:VCARD
 //   `.trim();
 
@@ -38,13 +38,13 @@ function downloadVCard() {
   const vCardData = `
 BEGIN:VCARD
 VERSION:3.0
-FN:MODAR SULAIMAN
+FN:HUSSAM OUSAMA ASAAD
 TITLE:Sales Manager
 ORG:RISE Properties
-TEL;TYPE=WORK,VOICE:+971557840193
-EMAIL:Modar@riseproperties.ae
+TEL;TYPE=WORK,VOICE:+971525572118‬‬‬
+EMAIL:Hussam@riseproperties.ae
 URL:http://www.riseproperties.ae
-ADR;TYPE=WORK:;;PO Box No 455979, Office NO.1607, Opal Tower, Business Bay;Dubai;;UAE
+ADR;TYPE=WORK:;;PO Box No 181702, Office NO.1607, Opal Tower, Business Bay;Dubai;;UAE
 END:VCARD
   `.trim();
 
