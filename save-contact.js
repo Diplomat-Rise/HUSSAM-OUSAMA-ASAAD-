e28cbb8cd3df861b627620d1da33bc0e023242a6
Diplomat-Rise <diploriseit@gmail.com>
2025-07-03 15:25:01 +0530

    Update save-contact.js

--- save-contact.js
+++ save-contact.js
@@ -1,38 +1,4 @@
 
-// function downloadVCard() {
-//   const vCardData = `
-// BEGIN:VCARD
-// VERSION:3.0
-// FN:HUSSAM OUSAMA ASAAD
-// TITLE:Sales Manager
-// ORG:RISE Properties
-// TEL;TYPE=WORK,VOICE:+971525572118‬‬‬
-// TEL;TYPE=WORK,VOICE:+971589826204
-// TEL;TYPE=WORK,VOICE:+97145665109
-// EMAIL:Hussam@riseproperties.ae
-// URL:http://www.riseproperties.ae
-// ADR;TYPE=WORK:;;PO Box No 181702, Office NO.1607, Opal Tower, Business Bay;Dubai;;UAE
-// END:VCARD
-//   `.trim();
-
-//   const blob = new Blob([vCardData], { type: 'text/vcard' });
-//   const url = URL.createObjectURL(blob);
-
-//   const a = document.createElement('a');
-//   a.href = url;
-//   a.download = 'HIBA ALLAH NABIL RAYES ALI.vcf';
-//   document.body.appendChild(a);
-//   a.click();
-//   document.body.removeChild(a);
-//   URL.revokeObjectURL(url);
-// }
-
-// // Attach to button
-// document.querySelector('.action-button[onclick*="download-vcard"]').addEventListener('click', function (e) {
-//   e.preventDefault();
-//   downloadVCard();
-// });
-
 
 function downloadVCard() {
   const vCardData = `
